f884779f20daf4cc0b3dcff2730a3010d8bbca6d
Adam Dimmick <66253578+awdimmick@users.noreply.github.com>
2021-11-27 09:27:36 +0000

    Create stack.c

---
+++ data_structures/stack.c
@@ -0,0 +1,47 @@
+#include "stack.h"
+#include <stdlib.h>
+
+Stack *new_stack(int size){
+
+    Stack *s = malloc(sizeof(Stack));
+    s->items = calloc(size, sizeof(int));
+    s->maxSize = size;
+    s->top = -1;
+
+    return s;
+}
+
+
+int peek(Stack *s){
+
+    if (!stack_empty(s)){
+        return s->items[s->top];
+    }
+    else {
+        return -1;
+    }
+}
+
+int pop (Stack *s){
+
+    if (!stack_empty(s)){
+        int item = s->items[s->top];
+        s->top--;
+        return item;
+    }
+}
+
+void push (int item, Stack *s){
+
+    if (!stack_full(s)){
+        s->top++;
+        s->items[s->top] = item;
+    }
+
+}
+bool stack_empty(Stack *s){
+    return s->top == -1;
+}
+bool stack_full(Stack *s){
+    return (s->top == s->maxSize - 1);
+}
