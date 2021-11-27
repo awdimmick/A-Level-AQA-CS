9c5ea1186c73ac57ca119bcd4159095b18f8a174
Adam Dimmick <66253578+awdimmick@users.noreply.github.com>
2021-11-27 09:28:02 +0000

    Create stack.h

---
+++ data_structures/stack.h
@@ -0,0 +1,20 @@
+#ifndef DATA_STRUCTURES_STACK_H
+#define DATA_STRUCTURES_STACK_H
+
+#include <stdbool.h>
+
+typedef struct stack{
+
+    int *items;
+    int maxSize, top;
+
+} Stack;
+
+Stack *new_stack(int size);
+int peek(Stack *s);
+int pop (Stack *s);
+void push (int item, Stack *s);
+bool stack_empty(Stack *s);
+bool stack_full(Stack *s);
+
+#endif //DATA_STRUCTURES_STACK_H
